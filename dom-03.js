03e21628e195df85e0d573022fbfdc8d98080d00
plaboncosta <plabonjoseph@gmail.com>
2020-03-18 23:51:34 +0600

    complete dom lesson 3

--- dom-03.js
+++ dom-03.js
@@ -22,3 +22,53 @@
     // console.log(e.shiftKey);
     // console.log(e.altKey);
 // });
+
+const eventCall = e => {
+    console.log(e.type);
+}
+
+// button.addEventListener('dblclick', eventCall);
+// button.addEventListener('mousedown', eventCall);
+// button.addEventListener('mouseup', eventCall);
+// button.addEventListener('mouseenter', eventCall);
+// button.addEventListener('mouseleave', eventCall);
+// button.addEventListener('mousemove', eventCall);
+// button.addEventListener('mouseout', eventCall);
+// button.addEventListener('mouseover', eventCall);
+
+// let eventDiv = document.getElementById('event-check');
+// eventDiv.addEventListener('mouseenter', eventCall); // Only above parent node
+// eventDiv.addEventListener('mouseover', eventCall); // also for chlid node
+// eventDiv.addEventListener('mouseleave', eventCall); // Only above parent node
+// eventDiv.addEventListener('mouseout', eventCall); // also for chlid node
+
+const inputEventCall = e => {
+    console.log(e.type);
+    console.log(e.target.value);
+}
+
+// let input = document.querySelector('input[type="text"]');
+// input.addEventListener('keydown', inputEventCall);
+// input.addEventListener('keyup', inputEventCall);
+// input.addEventListener('keypress', inputEventCall);
+// input.addEventListener('focus', inputEventCall);
+// input.addEventListener('blur', inputEventCall);
+// input.addEventListener('cut', inputEventCall);
+// input.addEventListener('paste', inputEventCall);
+// input.addEventListener('input', inputEventCall);
+
+// let select = document.querySelector('select');
+// select.addEventListener('change', inputEventCall);
+// select.addEventListener('input', inputEventCall);
+
+
+let form = document.querySelector('#todo-form');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let value = {};
+    for(let i = 0; i < form.length; i++) {
+        if (form[i].name) Object.assign(value, {[form[i].name]: form[i].value});
+    }
+
+    console.log(value);
+});

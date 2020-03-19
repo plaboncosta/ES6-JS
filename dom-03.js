2db7a21e31db10d1d056f4a1d7ca8807bcfb2bcc
plaboncosta <plabonjoseph@gmail.com>
2020-03-20 00:57:15 +0600

    dom lesson 4 successfully done

--- dom-03.js
+++ dom-03.js
@@ -63,6 +63,7 @@ const inputEventCall = e => {
 
 
 let form = document.querySelector('#todo-form');
+let list = document.getElementById('todo-item-ul');
 form.addEventListener('submit', e => {
     e.preventDefault();
     let dataSet = {};
@@ -70,13 +71,40 @@ form.addEventListener('submit', e => {
         if (form[i].name) Object.assign(dataSet, {[form[i].name]: form[i].value});
     }
 
-    let list = document.getElementById('todo-item-ul');
     let beforeLi = document.querySelector('.todo-item-li:first-child');
     let insertLi = document.createElement('li');
     insertLi.className = 'todo-item-li';
     const text = document.createTextNode(dataSet.item);
     insertLi.appendChild(text);
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'delete-btn';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    insertLi.appendChild(deleteBtn);
     
     list.insertBefore(insertLi, beforeLi);
     document.getElementById('item-list-count').innerText = `Item List (${list.children.length})`;
+    document.querySelector('input[name="item"]').value = '';
 });
+
+
+list.addEventListener('click', e => {
+    if(!e.target.classList.contains('delete-btn')) return;
+    if(confirm('Are you sure want to delete this item?')) list.removeChild(e.target.parentElement);
+    document.getElementById('item-list-count').innerText = `Item List (${list.children.length})`;
+}); 
+
+let searchBox = document.getElementById('search-box');
+searchBox.addEventListener('keyup', e => {
+    const searchText = e.target.value.toLowerCase();
+    // const items = list.getElementsByTagName('li');
+    Array.from(list.children).forEach(item => {
+        let listText = item.firstChild.textContent.toLowerCase();
+        // if(listText.indexOf(searchText) != -1) item.style.display = 'block';
+        // else item.style.display = 'none';
+        if(listText.includes(searchText)) item.style.display = 'block';
+        else item.style.display = 'none';
+    });
+
+    
+});
+
